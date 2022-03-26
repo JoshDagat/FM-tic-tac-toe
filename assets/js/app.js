c3a08f8e6a05edf7b96ef9b6d343291e6cb09fb9
JosuerBague <josuer.online.work@gmail.com>
2022-03-26 21:07:55 +0800

    Troubleshooting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
-import { checkWinner } from "./_checkWinner.js";
 import { gameState } from "./_global.js";
 import { highlightSelectedToken } from "./_highlightSelectedToken.js";
 import { newGame } from "./_newGame.js";
-import { endRound } from "./_endRound.js";
 import { markPlayerCell } from "./_markPlayerCell.js";
 
 const tokens = document.querySelectorAll(".token");
@@ -20,17 +18,3 @@ startMenuBtns.forEach((btn) => btn.addEventListener("click", newGame));
 const cells = document.querySelectorAll(".play-cell");
 
 cells.forEach((cell) => cell.addEventListener("click", markPlayerCell));
-
-const mainGame = document.querySelector(".main-game");
-
-function markCell(e) {
-  if (gameState.turn === 1) {
-    e.target.setAttribute("data-value", 1);
-  } else {
-    e.target.setAttribute("data-value", 2);
-  }
-}
-
-function disableCell(e) {
-  e.target.style.pointerEvents = "none";
-}
